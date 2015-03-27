a4d411eecdee9b3623f5f4086eb11f64fb8ddf9d
167504 <167504@HSS-101-14.westwindsor.k12.nj.us>
2015-03-27 12:33:51 -0400

    stuff

--- src/peppers/Capcasin.java
+++ src/peppers/Capcasin.java
@@ -1,10 +1,15 @@
 package peppers;
 
+import lejos.nxt.*;
+
 public class Capcasin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello mr.horn");
+		Motor.B.forward();
+		Motor.C.forward();
+		Button.waitForAnyPress();
 	}
 
 }
