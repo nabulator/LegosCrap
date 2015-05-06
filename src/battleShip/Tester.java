9e4ff0d6f121ab7e3086175cf8c33cfbe4b92d55
fjoe <francisfull@gmail.com>
2015-05-06 14:43:00 -0400

    tracker

--- src/battleShip/Tester.java
+++ src/battleShip/Tester.java
@@ -12,12 +12,40 @@ import lejos.robotics.Color;
  */
 public class Tester 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		//Player test = new Player();
 		//test.init();
 		//test.myTurn();
-		System.out.println(Sound.playSample(new File("FF9.wav")));
-		Button.waitForAnyPress();
+//		System.out.println(Sound.playSample(new File("FF9.wav")));
+		
+		while(true)
+		{
+			
+
+			if(Button.ENTER.isDown())
+			{
+				Motor.B.rotate(50);
+				Motor.C.rotate(50);
+			}
+			
+			if(Button.RIGHT.isDown())
+			{
+				Motor.B.forward();
+				Motor.C.forward();
+			}
+			if(Button.LEFT.isDown())
+			{
+				System.out.println("B: tacho: " + Motor.B.getTachoCount() + " position: " + Motor.B.getPosition());
+				System.out.println("C: tacho: " + Motor.C.getTachoCount() + " position: " + Motor.C.getPosition());
+				Thread.sleep(400);
+			}
+			if(Button.ESCAPE.isDown() )
+			{
+				Motor.B.resetTachoCount();
+				Motor.C.resetTachoCount();
+			}
+		}
+//		Button.waitForAnyPress();
 	}
 }
--- src/mineMapping/SomethingRel.java
+++ src/mineMapping/SomethingRel.java
@@ -1,5 +1,8 @@
 package mineMapping;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 import lejos.nxt.*;
 
 /**
@@ -12,6 +15,10 @@ public class SomethingRel {
 
 	private static NXTRegulatedMotor rightWheel = Motor.B;
 	private static NXTRegulatedMotor leftWheel = Motor.C;
+	private static ArrayList<String> data;
+	
+	public static Point pos;
+	public static char dir;
 	
 	public static void main(String[] args) throws InterruptedException 
 	{
@@ -24,28 +31,87 @@ public class SomethingRel {
 		final int MIN_DIS_FROM_WALL = 10;
 		
 		moveForward();
+		
+		data = new ArrayList<String>();
+		pos = new Point(0, 0);
+		dir = 'u';
+		
 		while(!finished())
 		{
 			if(uss.getDistance() > MIN_DIS_FROM_WALL)
 			{
+				changeData('l');
 				turnLeft();
 				Thread.sleep(1000);
 			}
 			
 			else if(ts.isPressed())
 			{
+				changeData('r');
 				turnRight();
 				Thread.sleep(1000);
 			}
 
 			moveForward();
 		}
-
+	}
+	
+	public static void changeData(char turn)
+	{
+		int distance = Motor.B.getTachoCount();
+		data.add(String.valueOf(distance));
+		
+		data.add(String.valueOf(turn));
+		
+		int x = pos.x;
+		int y = pos.y;
+		
+		if(dir == 'u')
+			y+= distance;
+		else if(dir == 'd')
+			y-= distance;
+		else if(dir == 'r')
+			y+= distance;
+		else if(dir == 'l')
+			y-= distance;
+		
+		changeDir(turn);
+		pos = new Point(x, y);
+	}
+	
+	private static void changeDir(char turn)
+	{
+		if(turn == 'l')
+		{
+			if(dir == 'u')
+				dir = 'l';
+			else if(dir == 'l')
+				dir = 'd';
+			else if(dir == 'd')
+				dir = 'r';
+			else if(dir == 'r')
+				dir = 'u';
+		}
+		else if(turn == 'r')
+		{
+			if(dir == 'u')
+				dir = 'r';
+			else if(dir == 'r')
+				dir = 'd';
+			else if(dir == 'd')
+				dir = 'l';
+			else if(dir == 'l')
+				dir = 'u';
+		}
 	}
 
 	public static boolean finished()
 	{
-		return false;
+		final int STARTING_RADIUS= 180;
+		int x = pos.x;
+		int y = pos.y;
+		
+		return STARTING_RADIUS <= Math.sqrt((x*x) - (y*y));
 	}
 	
 	final static int DEGREES_FOR_TURN = 200;
@@ -60,6 +126,8 @@ public class SomethingRel {
 		leftWheel.rotate(-DEGREES_FOR_TURN, true);
 		rightWheel.rotate(DEGREES_FOR_TURN, false);
 		
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 	}
 	
 	public static void turnRight()
@@ -73,6 +141,8 @@ public class SomethingRel {
 		leftWheel.rotate(DEGREES_FOR_TURN, true);
 		rightWheel.rotate(-DEGREES_FOR_TURN, false);
 		
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 	}
 	
 	public static void stopMoving()
